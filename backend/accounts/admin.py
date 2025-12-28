74c8db1616f5ff4a99607ecbd06748ab6d165532
Johan Joseph <iamstevefromspace@gmail.com>
2025-12-28 15:53:47 +0530

    Finalize custom user model and access roles

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class Useradmin(admin.ModelAdmin):
+    list_display=('username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_filter=('role', 'is_staff', 'is_superuser')
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    STAFF = 'STAFF'
+    STUDENT = 'STUDENT'
+
+    ROLE_CHOICES =[
+        (STAFF, 'Fcaulty/Staff'),
+        (STUDENT, 'Student'),
+    ]
+
+    role=models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=STAFF
+    )
+
+def __str__(self):
+    return self.username
